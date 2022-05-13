9cf7e92710aa7c5b7d8a67c3f5e33628c7ce4833
Austin Shen <austin.shen@csiro.au>
2022-05-13 05:33:40 +0000

    removing unique constraints for source finding data

--- src/02-source-finding.sql
+++ src/02-source-finding.sql
@@ -10,15 +10,15 @@ CREATE TABLE wallaby.run (
   "id" BIGSERIAL PRIMARY KEY
 );
 ALTER TABLE wallaby.run ADD COLUMN "name" varchar NOT NULL UNIQUE;
-ALTER TABLE wallaby.run ADD COLUMN "sanity_thresholds" jsonb NOT NULL UNIQUE;
+ALTER TABLE wallaby.run ADD COLUMN "sanity_thresholds" jsonb NOT NULL;
 
 
 CREATE TABLE wallaby.instance (
   "id" BIGSERIAL PRIMARY KEY
 );
-ALTER TABLE wallaby.instance ADD COLUMN "run_id" BIGINT NOT NULL UNIQUE;
+ALTER TABLE wallaby.instance ADD COLUMN "run_id" BIGINT NOT NULL;
 ALTER TABLE wallaby.instance ADD COLUMN "filename" varchar NOT NULL UNIQUE;
-ALTER TABLE wallaby.instance ADD COLUMN "boundary" integer[] NOT NULL UNIQUE;
+ALTER TABLE wallaby.instance ADD COLUMN "boundary" integer[] NOT NULL;
 ALTER TABLE wallaby.instance ADD COLUMN "run_date" timestamp without time zone NOT NULL;
 ALTER TABLE wallaby.instance ADD COLUMN "flag_log" bytea;
 ALTER TABLE wallaby.instance ADD COLUMN "reliability_plot" bytea;
@@ -34,24 +34,24 @@ ALTER TABLE wallaby.instance ADD FOREIGN KEY ("run_id") REFERENCES wallaby.run (
 CREATE TABLE wallaby.detection (
   "id" BIGSERIAL PRIMARY KEY
 );
-ALTER TABLE wallaby.detection ADD COLUMN "instance_id" BIGINT NOT NULL UNIQUE;
-ALTER TABLE wallaby.detection ADD COLUMN "run_id" BIGINT NOT NULL UNIQUE;
-ALTER TABLE wallaby.detection ADD COLUMN "name" varchar NOT NULL UNIQUE;
+ALTER TABLE wallaby.detection ADD COLUMN "instance_id" BIGINT NOT NULL;
+ALTER TABLE wallaby.detection ADD COLUMN "run_id" BIGINT NOT NULL;
+ALTER TABLE wallaby.detection ADD COLUMN "name" varchar NOT NULL;
 ALTER TABLE wallaby.detection ADD COLUMN "access_url" varchar NOT NULL;
 ALTER TABLE wallaby.detection ADD COLUMN "access_format" varchar DEFAULT 'application/x-votable+xml;content=datalink' NOT NULL;
-ALTER TABLE wallaby.detection ADD COLUMN "x" double precision NOT NULL UNIQUE;
-ALTER TABLE wallaby.detection ADD COLUMN "y" double precision NOT NULL UNIQUE;
-ALTER TABLE wallaby.detection ADD COLUMN "z" double precision NOT NULL UNIQUE;
-ALTER TABLE wallaby.detection ADD COLUMN "x_min" numeric NOT NULL UNIQUE;
-ALTER TABLE wallaby.detection ADD COLUMN "x_max" numeric NOT NULL UNIQUE;
-ALTER TABLE wallaby.detection ADD COLUMN "y_min" numeric NOT NULL UNIQUE;
-ALTER TABLE wallaby.detection ADD COLUMN "y_max" numeric NOT NULL UNIQUE;
-ALTER TABLE wallaby.detection ADD COLUMN "z_min" numeric NOT NULL UNIQUE;
-ALTER TABLE wallaby.detection ADD COLUMN "z_max" numeric NOT NULL UNIQUE;
-ALTER TABLE wallaby.detection ADD COLUMN "n_pix" numeric NOT NULL UNIQUE;
-ALTER TABLE wallaby.detection ADD COLUMN "f_min" double precision NOT NULL UNIQUE;
-ALTER TABLE wallaby.detection ADD COLUMN "f_max" double precision NOT NULL UNIQUE;
-ALTER TABLE wallaby.detection ADD COLUMN "f_sum" double precision NOT NULL UNIQUE;
+ALTER TABLE wallaby.detection ADD COLUMN "x" double precision NOT NULL;
+ALTER TABLE wallaby.detection ADD COLUMN "y" double precision NOT NULL;
+ALTER TABLE wallaby.detection ADD COLUMN "z" double precision NOT NULL;
+ALTER TABLE wallaby.detection ADD COLUMN "x_min" numeric NOT NULL;
+ALTER TABLE wallaby.detection ADD COLUMN "x_max" numeric NOT NULL;
+ALTER TABLE wallaby.detection ADD COLUMN "y_min" numeric NOT NULL;
+ALTER TABLE wallaby.detection ADD COLUMN "y_max" numeric NOT NULL;
+ALTER TABLE wallaby.detection ADD COLUMN "z_min" numeric NOT NULL;
+ALTER TABLE wallaby.detection ADD COLUMN "z_max" numeric NOT NULL;
+ALTER TABLE wallaby.detection ADD COLUMN "n_pix" numeric NOT NULL;
+ALTER TABLE wallaby.detection ADD COLUMN "f_min" double precision NOT NULL;
+ALTER TABLE wallaby.detection ADD COLUMN "f_max" double precision NOT NULL;
+ALTER TABLE wallaby.detection ADD COLUMN "f_sum" double precision NOT NULL;
 ALTER TABLE wallaby.detection ADD COLUMN "rel" double precision;
 ALTER TABLE wallaby.detection ADD COLUMN "rms" double precision NOT NULL;
 ALTER TABLE wallaby.detection ADD COLUMN "w20" double precision NOT NULL;
@@ -97,7 +97,6 @@ CREATE TABLE wallaby.product (
   "id" BIGSERIAL PRIMARY KEY
 );
 ALTER TABLE wallaby.product ADD COLUMN "detection_id" BIGINT NOT NULL UNIQUE;
-ALTER TABLE wallaby.product ADD FOREIGN KEY ("detection_id") REFERENCES wallaby.detection ("id") ON DELETE CASCADE;
 ALTER TABLE wallaby.product ADD COLUMN "cube" bytea;
 ALTER TABLE wallaby.product ADD COLUMN "mask" bytea;
 ALTER TABLE wallaby.product ADD COLUMN "mom0" bytea;
@@ -107,6 +106,7 @@ ALTER TABLE wallaby.product ADD COLUMN "snr" bytea NULL;
 ALTER TABLE wallaby.product ADD COLUMN "chan" bytea NULL;
 ALTER TABLE wallaby.product ADD COLUMN "spec" bytea;
 ALTER TABLE wallaby.product ADD COLUMN "summary" bytea NULL;
+ALTER TABLE wallaby.product ADD FOREIGN KEY ("detection_id") REFERENCES wallaby.detection ("id") ON DELETE CASCADE;
 
 
 CREATE TABLE wallaby.source (
@@ -132,7 +132,7 @@ CREATE TABLE wallaby.comment (
 ALTER TABLE wallaby.comment ADD COLUMN "comment" text NOT NULL;
 ALTER TABLE wallaby.comment ADD COLUMN "author" text NOT NULL;
 ALTER TABLE wallaby.comment ADD COLUMN "detection_id" bigint NOT NULL;
-ALTER TABLE wallaby.comment ADD COLUMN "updated_at" timestamp without time zone  NOT NULL;
+ALTER TABLE wallaby.comment ADD COLUMN "updated_at" timestamp without time zone NOT NULL;
 ALTER TABLE wallaby.comment ADD FOREIGN KEY ("detection_id") REFERENCES wallaby.detection ("id") ON UPDATE CASCADE ON DELETE CASCADE;
 
 
@@ -143,18 +143,6 @@ ALTER TABLE wallaby.tag ADD COLUMN "name" varchar NOT NULL UNIQUE;
 ALTER TABLE wallaby.tag ADD COLUMN "description" text;
 ALTER TABLE wallaby.tag ADD COLUMN "added_at" timestamp without time zone NOT NULL;
 ALTER TABLE wallaby.tag ALTER COLUMN added_at SET DEFAULT now();
-
-
-CREATE TABLE wallaby.tag_detection (
-  "id" BIGSERIAL PRIMARY KEY
-);
-ALTER TABLE wallaby.tag_detection ADD COLUMN "tag_id" bigint NOT NULL;
-ALTER TABLE wallaby.tag_detection ADD COLUMN "detection_id" bigint NOT NULL;
-ALTER TABLE wallaby.tag_detection ADD COLUMN "author" text NOT NULL;
-ALTER TABLE wallaby.tag_detection ADD COLUMN "added_at" timestamp without time zone NOT NULL;
-ALTER TABLE wallaby.tag_detection ALTER COLUMN added_at SET DEFAULT now();
-ALTER TABLE wallaby.tag_detection ADD FOREIGN KEY ("tag_id") REFERENCES wallaby.tag ("id") ON UPDATE CASCADE ON DELETE CASCADE;
-ALTER TABLE wallaby.tag_detection ADD FOREIGN KEY ("detection_id") REFERENCES wallaby.detection ("id") ON UPDATE CASCADE ON DELETE CASCADE;
 
 
 -- Tag source detection for release data
