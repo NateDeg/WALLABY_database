9774cd420bd38fb3fa170cb03131ec4665f3f018
Austin Shen <austin.shen@csiro.au>
2022-08-03 09:52:28 +0000

    WAL-137: fixed lol easy

--- orm/operations/models.py
+++ orm/operations/models.py
@@ -40,8 +40,8 @@ class Tile(models.Model):
     identifier = models.CharField(max_length=64, blank=True, null=True)
     description = models.CharField(max_length=512, blank=True, null=True)
     phase = models.CharField(max_length=512, blank=True, null=True)
-    footprint_A = models.ForeignKey(Observation, blank=True, null=True, on_delete=models.DO_NOTHING)
-    footprint_B = models.ForeignKey(Observation, blank=True, null=True, on_delete=models.DO_NOTHING)
+    footprint_A = models.ForeignKey(Observation, db_column='footprint_A', blank=True, null=True, on_delete=models.DO_NOTHING)
+    footprint_B = models.ForeignKey(Observation, db_column='footprint_B', blank=True, null=True, on_delete=models.DO_NOTHING)
     image_cube_file = models.CharField(max_length=512, blank=True, null=True)
     weights_cube_file = models.CharField(max_length=512, blank=True, null=True)
 
