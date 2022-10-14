cd6d6dd7e18b1a4f4ac17d455380670fc1564213
Austin Shen <austin.shen@csiro.au>
2022-10-14 02:07:30 +0000

    Kinematic model products + rename

--- src/03-kinematics.sql
+++ src/03-kinematics.sql
@@ -2,38 +2,66 @@
 
 -- Kinematic model catalogue
 CREATE TABLE wallaby.kinematic_model (
-  "ID" BIGSERIAL PRIMARY KEY,
-  "Name" varchar NOT NULL,
-  "RA" double precision NOT NULL,
-  "DEC" double precision NOT NULL,
+  "id" BIGSERIAL PRIMARY KEY,
+  "name" varchar NOT NULL,
+  "ra" double precision NOT NULL,
+  "dec" double precision NOT NULL,
   "freq" double precision NOT NULL,
   "team_release" varchar NOT NULL,
   "team_release_kin" varchar NOT NULL,
-  "Vsys_model" double precision NOT NULL,
-  "e_Vsys_model" double precision NOT NULL,
-  "X_model" double precision NOT NULL,
-  "e_X_model" double precision NOT NULL,
-  "Y_model" double precision NOT NULL,
-  "e_Y_model" double precision NOT NULL,
-  "RA_model" double precision NOT NULL,
-  "e_RA_model" double precision NOT NULL,
-  "DEC_model" double precision NOT NULL,
-  "e_DEC_model" double precision NOT NULL,
-  "Inc_model" double precision NOT NULL,
-  "e_Inc_model" double precision NOT NULL,
-  "PA_model" double precision NOT NULL,
-  "e_PA_model" double precision NOT NULL,
-  "PA_model_g" double precision NOT NULL,
-  "e_PA_model_g" double precision NOT NULL,
-  "QFlag_model" integer NOT NULL,
-  "Rad" varchar NOT NULL,
-  "Vrot_model" varchar NOT NULL,
-  "e_Vrot_model" varchar NOT NULL,
-  "e_Vrot_model_inc" varchar NOT NULL,
-  "Rad_SD" varchar NOT NULL,
-  "SD_model" varchar NOT NULL,
-  "SD_FO_model" varchar NOT NULL,
-  "e_SD_model" varchar NULL,
-  "e_SD_FO_model_inc" varchar NULL,
+  "vsys_model" double precision NOT NULL,
+  "e_vsys_model" double precision NOT NULL,
+  "x_model" double precision NOT NULL,
+  "e_x_model" double precision NOT NULL,
+  "y_model" double precision NOT NULL,
+  "e_y_model" double precision NOT NULL,
+  "ra_model" double precision NOT NULL,
+  "e_ra_model" double precision NOT NULL,
+  "dec_model" double precision NOT NULL,
+  "e_dec_model" double precision NOT NULL,
+  "inc_model" double precision NOT NULL,
+  "e_inc_model" double precision NOT NULL,
+  "pa_model" double precision NOT NULL,
+  "e_pa_model" double precision NOT NULL,
+  "pa_model_g" double precision NOT NULL,
+  "e_pa_model_g" double precision NOT NULL,
+  "qflag_model" integer NOT NULL,
+  "rad" varchar NOT NULL,
+  "vrot_model" varchar NOT NULL,
+  "e_vrot_model" varchar NOT NULL,
+  "e_vrot_model_inc" varchar NOT NULL,
+  "rad_sd" varchar NOT NULL,
+  "sd_model" varchar NOT NULL,
+  "sd_fo_model" varchar NOT NULL,
+  "e_sd_model" varchar NULL,
+  "e_sd_fo_model_inc" varchar NULL,
+  "r_hi" double precision NULL,
+  "v_disp" double precision NULL,
+  "v_rhi" double precision NULL,
+  "kinver" varchar NOT NULL,
 );
-ALTER TABLE wallaby.kinematic_model ADD FOREIGN KEY ("Name") REFERENCES wallaby.source ("name") ON DELETE CASCADE;
+ALTER TABLE wallaby.kinematic_model ADD FOREIGN KEY ("name") REFERENCES wallaby.source ("name") ON DELETE CASCADE;
+
+
+-- Kinematic model product files
+CREATE TABLE wallaby.wkapp_product (
+  "id" BIGSERIAL PRIMARY KEY
+);
+ALTER TABLE wallaby.wkapp_product ADD COLUMN "kinematic_model_id" BIGINT NOT NULL;
+ALTER TABLE wallaby.wkapp_product ADD COLUMN "baroloinput" bytea;
+ALTER TABLE wallaby.wkapp_product ADD COLUMN "barolomod" bytea;
+ALTER TABLE wallaby.wkapp_product ADD COLUMN "barolosurfdens" bytea;
+ALTER TABLE wallaby.wkapp_product ADD COLUMN "diagnosticplot" bytea;
+ALTER TABLE wallaby.wkapp_product ADD COLUMN "diffcube" bytea;
+ALTER TABLE wallaby.wkapp_product ADD COLUMN "fatinput" bytea;
+ALTER TABLE wallaby.wkapp_product ADD COLUMN "fatmod" bytea;
+ALTER TABLE wallaby.wkapp_product ADD COLUMN "fullresmodcube" bytea;
+ALTER TABLE wallaby.wkapp_product ADD COLUMN "fullresproccube" bytea;
+ALTER TABLE wallaby.wkapp_product ADD COLUMN "modcube" bytea;
+ALTER TABLE wallaby.wkapp_product ADD COLUMN "procdata" bytea;
+ALTER TABLE wallaby.wkapp_product ADD FOREIGN KEY ("kinematic_model_id") REFERENCES wallaby.kinematic_model ("id") ON DELETE CASCADE;
+
+
+CREATE TABLE wallaby.wrkp_product (
+  "id" BIGSERIAL PRIMARY KEY
+);
