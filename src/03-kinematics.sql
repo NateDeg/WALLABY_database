43614d3ccd3716b25e3e54c759818692db86b336
Austin Shen <austin.shen@csiro.au>
2022-10-17 04:00:23 +0000

    WAL-151: kinematic model update in database

--- src/03-kinematics.sql
+++ src/03-kinematics.sql
@@ -3,43 +3,43 @@
 -- Kinematic model catalogue
 CREATE TABLE wallaby.kinematic_model (
   "id" BIGSERIAL PRIMARY KEY,
-  "name" varchar NOT NULL,
-  "ra" double precision NOT NULL,
-  "dec" double precision NOT NULL,
-  "freq" double precision NOT NULL,
-  "team_release" varchar NOT NULL,
-  "team_release_kin" varchar NOT NULL,
-  "vsys_model" double precision NOT NULL,
-  "e_vsys_model" double precision NOT NULL,
-  "x_model" double precision NOT NULL,
-  "e_x_model" double precision NOT NULL,
-  "y_model" double precision NOT NULL,
-  "e_y_model" double precision NOT NULL,
-  "ra_model" double precision NOT NULL,
-  "e_ra_model" double precision NOT NULL,
-  "dec_model" double precision NOT NULL,
-  "e_dec_model" double precision NOT NULL,
-  "inc_model" double precision NOT NULL,
-  "e_inc_model" double precision NOT NULL,
-  "pa_model" double precision NOT NULL,
-  "e_pa_model" double precision NOT NULL,
-  "pa_model_g" double precision NOT NULL,
-  "e_pa_model_g" double precision NOT NULL,
-  "qflag_model" integer NOT NULL,
-  "rad" varchar NOT NULL,
-  "vrot_model" varchar NOT NULL,
-  "e_vrot_model" varchar NOT NULL,
-  "e_vrot_model_inc" varchar NOT NULL,
-  "rad_sd" varchar NOT NULL,
-  "sd_model" varchar NOT NULL,
-  "sd_fo_model" varchar NOT NULL,
-  "e_sd_model" varchar NULL,
-  "e_sd_fo_model_inc" varchar NULL,
-  "r_hi" double precision NULL,
-  "v_disp" double precision NULL,
-  "v_rhi" double precision NULL,
-  "kinver" varchar NOT NULL,
 );
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "name" varchar NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "ra" double precision NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "dec" double precision NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "freq" double precision NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "team_release" varchar NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "team_release_kin" varchar NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "vsys_model" double precision NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "e_vsys_model" double precision NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "x_model" double precision NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "e_x_model" double precision NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "y_model" double precision NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "e_y_model" double precision NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "ra_model" double precision NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "e_ra_model" double precision NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "dec_model" double precision NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "e_dec_model" double precision NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "inc_model" double precision NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "e_inc_model" double precision NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "pa_model" double precision NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "e_pa_model" double precision NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "pa_model_g" double precision NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "e_pa_model_g" double precision NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "qflag_model" integer NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "rad" varchar NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "vrot_model" varchar NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "e_vrot_model" varchar NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "e_vrot_model_inc" varchar NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "rad_sd" varchar NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "sd_model" varchar NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "sd_fo_model" varchar NOT NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "e_sd_model" varchar NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "e_sd_fo_model_inc" varchar NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "r_hi" double precision NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "v_disp" double precision NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "v_rhi" double precision NULL;
+ALTER TABLE wallaby.kinematic_model ADD COLUMN "kinver" varchar NULL;
 ALTER TABLE wallaby.kinematic_model ADD FOREIGN KEY ("name") REFERENCES wallaby.source ("name") ON DELETE CASCADE;
 
 
