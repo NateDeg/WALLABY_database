13454410d95bd49c0fc502ed24ad60b51402d138
Austin Shen <austin.shen@csiro.au>
2022-11-28 08:32:05 +0000

    fix

--- orm/kinematic_model/models.py
+++ orm/kinematic_model/models.py
@@ -9,7 +9,7 @@ def PostgresDecimalField(*args, **kwargs):
 
 class KinematicModel(models.Model):
     id = models.BigAutoField(primary_key=True)
-    name = models.ForeignKey(Source, to_field='name', on_delete=models.DO_NOTHING)
+    source = models.ForeignKey(Source, to_field='name', on_delete=models.DO_NOTHING)
     ra = PostgresDecimalField()
     dec = PostgresDecimalField()
     freq = PostgresDecimalField()
